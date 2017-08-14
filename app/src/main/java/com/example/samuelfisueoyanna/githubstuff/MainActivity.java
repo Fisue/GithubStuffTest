c930205edb90bfeb11afc9ab00ca0b1234e78f49
Samuel Oyanna <samueloyanna11@gmail.com>
2017-08-14 09:31:11 +0100

    Fixed error-on-purpose

--- app/src/main/java/com/example/samuelfisueoyanna/githubstuff/MainActivity.java
+++ app/src/main/java/com/example/samuelfisueoyanna/githubstuff/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
+        so i just want to add this to my project after undo
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
